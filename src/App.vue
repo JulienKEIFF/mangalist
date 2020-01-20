b44a3bcbb52a4672cfa9e3c0feea2f89375b1fdc
julien <j.keiff@hotmail.fr>
2020-01-20 18:20:58 +0000

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="teal" dark>
+      <div class="d-flex align-center">
+        <v-icon x-large @click.stop="drawer = !drawer">mdi-menu</v-icon>
+        <!-- <v-icon>mdi-cellphone-arrow-down</v-icon> -->
+        <h1>MANGALIST</h1>
+      </div>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary clipped id="menu">
+      <v-list dense>
+
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-icon>{{ item.icon }}</v-icon>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'app',
+  name: 'App',
+
   components: {
-    HelloWorld
+    HelloWorld,
+  },
+
+  data () {
+    return {
+      drawer: null,
+      items: [
+        { title: 'Home', icon: 'mdi-view-dashboard' },
+        { title: 'About', icon: 'mdi-help' },
+      ],
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="css" scoped>
+#menu{
+  top: 25px;
+}
+.v-overlay_scrim{
+  position: absolute;
+  top: 57px !important;
+}
+.v-navigation-drawer__content{
+  position: absolute;
+  top: 56px !important;
 }
 </style>
